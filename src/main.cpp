2d4a0920a11d8e6fbe3b963bff1e997ade95631c
Vetri <vetri.brainscience@gmail.com>
2025-05-21 20:16:31 +0530

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#define LED_PIN 1 //P1 on Digispark
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(100);
+  digitalWrite(LED_PIN, LOW);
+  delay(2500);
+}
